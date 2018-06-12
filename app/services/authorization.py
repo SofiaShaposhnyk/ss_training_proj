0a15e9618f2c17a1ef5118774972dbefe457b0fd
SofiaShaposhnyk <s.shaposhnyk@gmail.com>
2018-06-12 11:51:09 +0300

    fix app structure

--- app/services/authorization.py
+++ app/services/authorization.py
@@ -14,10 +14,10 @@ async def check_login_data(request):
     user_info = {
         'login': request.args.get('login'), 'password': request.args.get('password').encode('utf-8')
     }
-    hashed_info = hashlib.md5(user_info['password']).hexdigest()
+    hashed_pass = hashlib.md5(user_info['password']).hexdigest()
     stored_info = await db.get_user_by_login(request.args.get('login'))
     stored_pass = stored_info[0]['password_hash']
-    if hashed_info == stored_pass:
+    if hashed_pass == stored_pass:
         token = await get_token()
         await insert_redis(user_info['login'], token)
         await write_cookie(token)
